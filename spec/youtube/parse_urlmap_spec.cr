bbc7fd69b3391830a376f2a3311c9b7ad87bfb60
akiicat <aaaa1379@gmail.com>
2017-03-30 10:33:28 +0800

    convert all value to string type

--- spec/youtube/parse_urlmap_spec.cr
+++ spec/youtube/parse_urlmap_spec.cr
@@ -7,29 +7,29 @@ describe Youtube do
 
     a1 = [
       {
-        "itag"=>22, "quality"=>"hd720",
+        "itag"=>"22", "quality"=>"hd720",
         "s"=>"32E3D5A7E5C844B01F7E8049B3CDEC18DF37F5F0B.DFAD820780E2C111F65AE93FA69BBA2200775D0A",
         "type"=>"video/mp4; codecs=\"avc1.64001F, mp4a.40.2\"", "url"=>"https://r5---sn-ipoxu-un56.googlevideo.com/videoplayback?mm=31&mn=sn-ipoxu-un56&id=o-AApYzSAoVZcOSbj-yOws8sHDB4DHHKSEedU71E1ZFa4p&initcwndbps=2917500&ip=61.220.182.115&mt=1490753246&mv=m&ms=au&lmt=1481464250294058&dur=284.839&mime=video%2Fmp4&key=yt6&sparams=dur%2Cei%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Cratebypass%2Crequiressl%2Csource%2Cupn%2Cexpire&expire=1490774975&requiressl=yes&itag=22&ratebypass=yes&pl=24&source=youtube&ei=XxfbWLniCcKO4AKpkoXgDw&upn=QW9MWyooct4&ipbits=0&signature=45770022ABB96AF39EA56F111C2E087028DAFD.B0F5F73FD81CEDC3B9408E7F10B4D8C5E7A5D"
       },
       {
-        "itag"=>43,
+        "itag"=>"43",
         "quality"=>"medium",
         "s"=>"E679DFCDC5A78077CF27C99801C7CE8C96DF1A7FD.EB4B160AF4E664531D988A4763813674315B9D0B",
         "type"=>"video/webm; codecs=\"vp8.0, vorbis\"", "url"=>"https://r5---sn-ipoxu-un56.googlevideo.com/videoplayback?mm=31&clen=30245573&mn=sn-ipoxu-un56&id=o-AApYzSAoVZcOSbj-yOws8sHDB4DHHKSEedU71E1ZFa4p&gir=yes&initcwndbps=2917500&ip=61.220.182.115&mt=1490753246&mv=m&ms=au&lmt=1431610617920198&dur=0.000&mime=video%2Fwebm&key=yt6&sparams=clen%2Cdur%2Cei%2Cgir%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Cratebypass%2Crequiressl%2Csource%2Cupn%2Cexpire&expire=1490774975&requiressl=yes&itag=43&ratebypass=yes&pl=24&source=youtube&ei=XxfbWLniCcKO4AKpkoXgDw&upn=QW9MWyooct4&ipbits=0&signature=89B5134763183674A889D135466E4FA061B4BE.DF7A1FD69C8EC7C10899C72FC770D7A5CDCFD"
       },
       {
-        "itag"=>18,
+        "itag"=>"18",
         "quality"=>"medium",
         "s"=>"CF7CDDF2CE786F80B1BD35389925DEB2BF1854F07.DDC63AA73AFF9440E8410A3C2B0BB47743494645",
         "type"=>"video/mp4; codecs=\"avc1.42001E, mp4a.40.2\"", "url"=>"https://r5---sn-ipoxu-un56.googlevideo.com/videoplayback?mm=31&clen=25028066&mn=sn-ipoxu-un56&id=o-AApYzSAoVZcOSbj-yOws8sHDB4DHHKSEedU71E1ZFa4p&gir=yes&initcwndbps=2917500&ip=61.220.182.115&mt=1490753246&mv=m&ms=au&lmt=1481463181561351&dur=284.839&mime=video%2Fmp4&key=yt6&sparams=clen%2Cdur%2Cei%2Cgir%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Cratebypass%2Crequiressl%2Csource%2Cupn%2Cexpire&expire=1490774975&requiressl=yes&itag=18&ratebypass=yes&pl=24&source=youtube&ei=XxfbWLniCcKO4AKpkoXgDw&upn=QW9MWyooct4&ipbits=0&signature=649434774BB0B2C3A0148E0449FFA37AA36CDD.70F4581FB2BED52998353DB1B08F687EC2FDD"
       },
       {
-        "itag"=>36,
+        "itag"=>"36",
         "quality"=>"small",
         "s"=>"51DE85746891BB2A8BB33A1781ECFFA0889424E48.D756CBE017A1CC14C57F4DA423C30572AED7AC05",
         "type"=>"video/3gpp; codecs=\"mp4v.20.3, mp4a.40.2\"", "url"=>"https://r5---sn-ipoxu-un56.googlevideo.com/videoplayback?mm=31&clen=8135888&mn=sn-ipoxu-un56&id=o-AApYzSAoVZcOSbj-yOws8sHDB4DHHKSEedU71E1ZFa4p&gir=yes&initcwndbps=2917500&source=youtube&mt=1490753246&mv=m&ms=au&lmt=1431608743101235&dur=285.001&mime=video%2F3gpp&key=yt6&sparams=clen%2Cdur%2Cei%2Cgir%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Crequiressl%2Csource%2Cupn%2Cexpire&expire=1490774975&requiressl=yes&itag=36&pl=24&ip=61.220.182.115&ei=XxfbWLniCcKO4AKpkoXgDw&upn=QW9MWyooct4&ipbits=0&signature=BA7DEA27503C324AD4F75C41CC1A710EBC657D.84E4249880AFFCE1871A33BB8A2BC19864758"
       }, {
-        "itag"=>17,
+        "itag"=>"17",
         "quality"=>"small",
         "s"=>"7D49F5164178DAE719A41099CE2B22AE0258727EC.EBC5F68EE00D6A6391E94AD463860BCB1762AEBB",
         "type"=>"video/3gpp; codecs=\"mp4v.20.3, mp4a.40.2\"", "url"=>"https://r5---sn-ipoxu-un56.googlevideo.com/videoplayback?mm=31&clen=2910217&mn=sn-ipoxu-un56&id=o-AApYzSAoVZcOSbj-yOws8sHDB4DHHKSEedU71E1ZFa4p&gir=yes&initcwndbps=2917500&source=youtube&mt=1490753246&mv=m&ms=au&lmt=1431608692852431&dur=284.908&mime=video%2F3gpp&key=yt6&sparams=clen%2Cdur%2Cei%2Cgir%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Crequiressl%2Csource%2Cupn%2Cexpire&expire=1490774975&requiressl=yes&itag=17&pl=24&ip=61.220.182.115&ei=XxfbWLniCcKO4AKpkoXgDw&upn=QW9MWyooct4&ipbits=0&signature=DA2671BCB068364DA49E1936A6D00EE86F5CBE.CE7278520EA22B2EC99014A917EAE8714615F"
--- src/youtube/parse_urlmap.cr
+++ src/youtube/parse_urlmap.cr
@@ -3,21 +3,15 @@ require "../helpers/uri_helper"
 
 class Youtube
   def self.parse_urlmap(str)
-    str.split(",").map do |s|
-      hsh = Hash(String, String | Int32).new
-      h = URI.decode_www_form(s).to_h
-      h.each do |k, v|
+    str.to_s.split(",").map do |s|
+      hsh = URI.decode_www_form(s).to_h
+      hsh.each do |k, v|
+        hsh[k] = v
         case k
-        when "bitrate", "clen", "itag", "lmt", "projection_type"
-          hsh[k] = v.to_i
-        else
-          hsh[k] = v
-        end
-
-        if k == "s"
+        when "s"
           sig = apply_cipher(v)
           # hsh["decoded_signature"] = sig
-          hsh["url"] = "#{h["url"]}&signature=#{sig}"
+          hsh["url"] = "#{hsh["url"]}&signature=#{sig}"
         end
       end
       hsh
