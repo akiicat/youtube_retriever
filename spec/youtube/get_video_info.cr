2fe6b1812cd70976478b890de8181c059ad2deee
akiicat <aaaa1379@gmail.com>
2017-04-09 13:45:11 +0800

    clear old version

--- spec/youtube/get_video_info.cr
+++
@@ -1,8 +0,0 @@
-require "spec"
-require "../../src/youtube/get_video_info"
-
-describe Youtube do
-  it "#get_video_info" do
-    Youtube.get_video_info("iDfZua4IS4A").should_not be nil
-  end
-end
--- src/helpers/string_helper.cr
+++
@@ -1,7 +0,0 @@
-class String
-  def swap(x, y)
-    x = self.size + x if x < 0
-    y = self.size + y if y < 0
-    self.sub(x, self[y]).sub(y, self[x])
-  end
-end
