f09b23e3868fa0fad73e7744926fb8323758e1b3
akiicat <aaaa1379@gmail.com>
2017-04-20 12:02:12 +0800

    rename string_helper

--- src/helpers/string_helper.cr
+++ src/helpers/string_helper.cr
@@ -1,7 +1,13 @@
+require "json"
+
 class String
   def swap(x, y)
     x = self.size + x if x < 0
     y = self.size + y if y < 0
     self.sub(x, self[y]).sub(y, self[x])
+  end
+
+  def as_json
+    JSON.parse(self).to_s
   end
 end
