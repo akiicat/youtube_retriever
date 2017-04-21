042e79590a108817376d8d3959fd84ef0bae8f8b
akiicat <aaaa1379@gmail.com>
2017-04-21 23:10:23 +0800

    decipherer decrypt initialize with empty url

--- spec/extractor/decipherer_spec.cr
+++ spec/extractor/decipherer_spec.cr
@@ -11,15 +11,36 @@ describe Decipherer do
       Decipherer.new(url).decrypt(encrypted_sig).should eq decrypted_sig
     end
 
-    it "should have correct decoder steps" do
+    it "have correct decoder steps" do
       sig_id        = "vfl5-0t5t"
-      decoder_steps = "s1 w44 r s1"
+      steps         = "s1 w44 r s1"
       encrypted_sig = "334DC9BA30299ECF526EDB33AFA45C5C1534A745C.2934692D884D77A9C9B8C199B401A0FE3E52C1800"
 
       url = "https://www.youtube.com/yts/jsbin/player-en_US-#{sig_id}/base.js"
       decipher = Decipherer.new(url)
       decipher.decrypt(encrypted_sig)
-      decipher.decoder_steps.should eq decoder_steps
+      decipher.steps.should eq steps
+    end
+
+    it "initialize with empty url" do
+      sig_id        = "vfl5-0t5t"
+      encrypted_sig = "334DC9BA30299ECF526EDB33AFA45C5C1534A745C.2934692D884D77A9C9B8C199B401A0FE3E52C1800"
+      decrypted_sig = "081C25E3EF0A104B991C8B9C9A77D488D2963392.C547A4351C5C54AFA33BDE625FCE99203AB9CD44"
+
+      url = "https://www.youtube.com/yts/jsbin/player-en_US-#{sig_id}/base.js"
+      decipher = Decipherer.new
+      decipher.url = url
+      decipher.decrypt(encrypted_sig).should eq decrypted_sig
+    end
+
+    it "multiple times in same class" do
+      sig_id        = "vfl5-0t5t"
+      encrypted_sig = "334DC9BA30299ECF526EDB33AFA45C5C1534A745C.2934692D884D77A9C9B8C199B401A0FE3E52C1800"
+      decrypted_sig = "081C25E3EF0A104B991C8B9C9A77D488D2963392.C547A4351C5C54AFA33BDE625FCE99203AB9CD44"
+
+      url = "https://www.youtube.com/yts/jsbin/player-en_US-#{sig_id}/base.js"
+      decipher = Decipherer.new(url)
+      3.times { decipher.decrypt(encrypted_sig).should eq decrypted_sig }
     end
   end
 end
--- src/extractor/decipherer.cr
+++ src/extractor/decipherer.cr
@@ -1,18 +1,16 @@
 require "../config"
 
 class Decipherer
-  getter url   : String
-  getter steps : String
+  property url   : String
+  property steps : String
 
-  def initialize(url : String = "")
-    @url   = url
-    @steps = Interpreter.decode_steps(url)
+  def initialize(@url = "", @steps = "")
   end
 
   def decrypt(sig : String)
+    @steps = Interpreter.decode_steps(@url) if @steps.empty?
     @steps.split(" ").each do |op|
       op, n = op[0], op[1..-1].to_i? || 0
-      # puts op, n
       sig = case op
       when 'r' then sig.reverse
       when 's' then sig[n..-1]
