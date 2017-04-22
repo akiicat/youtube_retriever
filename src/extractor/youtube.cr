e6f604418b387062177d615372d1b94c271198d4
akiicat <aaaa1379@gmail.com>
2017-04-22 19:38:59 +0800

    kemal add other test

--- src/extractor/youtube.cr
+++ src/extractor/youtube.cr
@@ -28,13 +28,13 @@ class Youtube
   def real_extract
     LOG.info "Downloading video webpage: #{@video_id}"
     url = "#{@proto}://www.youtube.com/watch?v=#{@video_id}&gl=US&hl=en&has_verified=1&bpctr=9999999999"
-    @video_webpage     = InfoExtractor.download_webpage(url)
-    @player_url        = InfoExtractor.extract_player_url(@video_webpage)
+    @video_webpage     = download_webpage(url)
+    @player_url        = extract_player_url(@video_webpage)
     @decipher          = Decipherer.new(@player_url)
 
     LOG.info "Downloading video info: #{@video_id}"
-    @video_info        = InfoExtractor.get_video_info(@video_id)
-    @video_title       = @video_info["title"]?.to_s
+    @video_info        = get_video_info(@video_id)
+    @video_title       = video_info["title"]?.to_s
     @video_description = video_info["eow-description"]?.to_s
 
     encoded_url_map  = @video_info["url_encoded_fmt_stream_map"]?.to_s.split(",")
@@ -54,23 +54,6 @@ class Youtube
         url += "&ratebypass=yes"
       end
 
-      # 1705 	                if type_:
-      # 1706 	                    type_split = type_.split(';')
-      # 1707 	                    kind_ext = type_split[0].split('/')
-      # 1708 ->	                    if len(kind_ext) == 2:
-      # 1709 	                        kind, _ = kind_ext
-      # 1710 	                        dct['ext'] = mimetype2ext(type_split[0])
-      # 1711 	                        if kind in ('audio', 'video'):
-      # 1712 	                            codecs = None
-      # 1713 	                            for mobj in re.finditer(
-      # 1714 	                                    r'(?P<key>[a-zA-Z_-]+)=(?P<quote>["\']?)(?P<val>.+?)(?P=quote)(?:;|$)', type_):
-      # 1715 ->	                                if mobj.group('key') == 'codecs':
-      # 1716 	                                    codecs = mobj.group('val')
-      # 1717 	                                    break
-      # 1718 	                            if codecs:
-      # 1719 	                                dct.update(parse_codecs(codecs))
-      # 1720 	                formats.append(dct)
-      # {u'vcodec': u'avc1.64001F', u'acodec': u'mp4a.40.2'}
 
       url_data["url"] = url
 
--- src/youtube.cr
+++ src/youtube.cr
@@ -1,19 +1,39 @@
 require "kemal"
 require "./config"
 
-before_get "/" do |env|
-  env.response.content_type = "application/json; charset=utf-8"
-end
+ERROR = { status: "fail" }
 
 get "/" do |env|
+  env.response.content_type = "application/json; charset=utf-8"
   Youtube.dump_json("iDfZua4IS4A").to_json
 end
 
 get "/python" do |env|
+  env.response.content_type = "application/json; charset=utf-8"
   io = IO::Memory.new
   Process.run("youtube-dl", ["-j", "iDfZua4IS4A"], output: io)
   JSON.parse(io.to_s).to_json
 end
 
+get "/vevo" do |env|
+  env.response.content_type = "application/json; charset=utf-8"
+  Youtube.dump_json("tvTRZJ-4EyI").to_json
+end
+
+get "/head" do |env|
+  env.response.content_type = "application/json; charset=utf-8"
+  a = "https://r8---sn-ipoxu-un5d.googlevideo.com/videoplayback?key=yt6&ip=61.220.182.115&mn=sn-ipoxu-un5d&mm=31&ms=au&ipbits=0&pl=24&mv=m&mt=1492746088&sparams=dur%2Cei%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Cratebypass%2Crequiressl%2Csource%2Cupn%2Cexpire&requiressl=yes&ei=wn_5WIj6MMbC4QLPjqfgCg&dur=183.646&mime=video%2Fmp4&itag=22&expire=1492767778&upn=lAO3p86liWI&lmt=1490906644132788&ratebypass=yes&id=o-APHph8qjXg5M1HpyIgVYcmI8BR2Xuwueo6brxDRqZrCw&initcwndbps=3042500&source=youtube&signature=54F2D56F999AE929356D8EC615A3CB404AFDEA13.CEF23004A5BE049913276FD3D6B60C548782B0BD"
+
+  HTTP::Client.head(a).status_code
+end
+
+get "/error" do |env|
+  env.response.content_type = "application/json; charset=utf-8"
+  begin
+    Youtube.dump_json("iDfZua4IsS4A").to_json
+  rescue
+    ERROR.to_json
+  end
+end
 
 Kemal.run
