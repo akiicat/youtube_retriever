224f8a5b8ae71568d74ab27651cfed414e5d41e0
Akiicat <aaaa1379@gmail.com>
2017-04-23 00:44:43 +0800

    extract encoded_urls and package

--- src/extractor/decipherer.cr
+++ src/extractor/decipherer.cr
@@ -21,4 +21,31 @@ class Decipherer
     end
     sig
   end
+
+  def package(encoded_urls : String | Nil = nil)
+    rtn = [] of Hash(Symbol, String)
+    encoded_urls.to_s.split(",").each do |url_data_str|
+      url_data = URI.decode_www_form(url_data_str)
+
+      itag    = url_data["itag"]?
+      url     = url_data["url"]?
+      sig     = url_data["sig"]?
+      s       = url_data["s"]?
+      bitrate = url_data["bitrate"]?
+
+      next if !itag || !url
+
+      url += "&signature=#{sig}"        if sig
+      url += "&signature=#{decrypt(s)}" if s
+      url += "&ratebypass=yes"          if /ratebypass/ !~ url
+
+      hsh = Codecs.format(itag)
+      hsh[:url] = url
+      hsh[:video_bitrate] = bitrate.as(String) if hsh[:comment] == "video only"
+      hsh[:audio_bitrate] = bitrate.as(String) if hsh[:comment] == "audio only"
+
+      rtn.push hsh
+    end
+    rtn
+  end
 end
--- src/extractor/youtube.cr
+++ src/extractor/youtube.cr
@@ -1,63 +1,38 @@
 require "./info_extractor"
 
 class Youtube
-  property video_id = ""
-  property video_info = Hash(String, JSON::Type).new
-  property video_webpage = ""
-  property embed_webpage = ""
-  property video_title = ""
-  property video_description = ""
-  property proto = "https"
-  property player_url = ""
-  property encoded_url_map = [] of Hash(String, String)
-  property decipher = Decipherer.allocate
+  property video_id : String
+  property proto : String
+  property encoded_url_map = [] of Hash(Symbol, String)
+  property adaptive_fmts = [] of Hash(Symbol, String)
 
   include InfoExtractor
 
   def initialize(url : String)
     @video_id = extract_id(url)
+    @proto = "https"
   end
 
   def self.dump_json(url : String)
     a = new(url)
     a.real_extract
-    a.encoded_url_map
+    a.encoded_url_map + a.adaptive_fmts
   end
 
   # url = "https://www.youtube.com/watch?v=iDfZua4IS4A"
   def real_extract
     LOG.info "Downloading video webpage: #{@video_id}"
     url = "#{@proto}://www.youtube.com/watch?v=#{@video_id}&gl=US&hl=en&has_verified=1&bpctr=9999999999"
-    @video_webpage     = download_webpage(url)
-    @player_url        = extract_player_url(@video_webpage)
-    @decipher          = Decipherer.new(@player_url)
+    video_webpage     = download_webpage(url)
+    player_url        = extract_player_url(video_webpage)
+    decipher          = Decipherer.new(player_url)
 
     LOG.info "Downloading video info: #{@video_id}"
-    @video_info        = get_video_info(@video_id)
-    @video_title       = video_info["title"]?.to_s
-    @video_description = video_info["eow-description"]?.to_s
+    video_info        = get_video_info(@video_id)
+    video_title       = video_info["title"]?.to_s
+    video_description = video_info["eow-description"]?.to_s
 
-    encoded_url_map  = @video_info["url_encoded_fmt_stream_map"]?.to_s.split(",")
-    encoded_url_map += @video_info["adaptive_fmts"]?.to_s.split(",")
-    encoded_url_map.each do |url_data_str|
-			url_data_str
-      url_data = URI.decode_www_form(url_data_str)
-      next if !url_data.has_key? "itag" || !url_data.has_key? "url"
-      url = url_data["url"]
-
-      if url_data.has_key? "sig"
-        url += "&signature=" + url_data["sig"]
-      elsif url_data.has_key? "s"
-        url += "&signature=" + @decipher.decrypt(url_data["s"]).to_s
-      end
-      if /ratebypass/ !~ url
-        url += "&ratebypass=yes"
-      end
-
-
-      url_data["url"] = url
-
-      @encoded_url_map.push url_data
-    end
+    @encoded_url_map  = decipher.package(video_info["url_encoded_fmt_stream_map"]?.to_s)
+    @adaptive_fmts    = decipher.package(video_info["adaptive_fmts"]?.to_s)
   end
 end
